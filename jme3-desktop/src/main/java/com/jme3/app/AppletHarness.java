cdabf8e4025afabfcbe67dee05c3c871d04698bf
zaziba.ahmed <ahmedzaziba@gmail.com>
2024-12-02 02:35:35 -0500

    Issue#155-fixed Insecure Configuration vulnerability

--- jme3-desktop/src/main/java/com/jme3/app/AppletHarness.java
+++ jme3-desktop/src/main/java/com/jme3/app/AppletHarness.java
@@ -47,6 +47,7 @@ import java.net.URL;
 import java.util.HashMap;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import java.util.logging.Logger;
 
 /**
  * @author Kirill Vainer
